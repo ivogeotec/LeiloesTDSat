7dc93d22c3849942d86128c62fa8c4e51cb0b44f
Ivo <antonioivo92@gmail.com>
2026-08-29 11:42:47 -0300

    Commit #2: listagem de produtos funcionando

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -140,16 +143,26 @@ public class cadastroVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_cadastroNomeActionPerformed
 
     private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
-        ProdutosDTO produto = new ProdutosDTO();
-        String nome = cadastroNome.getText();
-        String valor = cadastroValor.getText();
-        String status = "A Venda";
-        produto.setNome(nome);
-        produto.setValor(Integer.parseInt(valor));
-        produto.setStatus(status);
-        
-        ProdutosDAO produtodao = new ProdutosDAO();
-        produtodao.cadastrarProduto(produto);
+ProdutosDTO produto = new ProdutosDTO();
+String nome = cadastroNome.getText();
+String valor = cadastroValor.getText();
+String status = "A Venda";
+
+produto.setNome(nome);
+produto.setValor(Integer.parseInt(valor));
+produto.setStatus(status);
+
+ProdutosDAO produtodao = new ProdutosDAO();
+int resposta = produtodao.cadastrarProduto(produto);
+
+if (resposta == 1) {
+    JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+    cadastroNome.setText("");
+    cadastroValor.setText("");
+    cadastroNome.requestFocus();
+} else {
+    JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto.");
+}
         
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
