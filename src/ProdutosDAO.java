e38669f0af3942cc5b03fadf3ecb347fd56a281a
Ivo <antonioivo92@gmail.com>
2026-09-03 10:57:00 -0300

    Commit #1: adicionado o botao Vender

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,7 +12,7 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public int cadastrarProduto (ProdutosDTO produto){
+    public int cadastrarProduto(ProdutosDTO produto) {
         int status;
         conn = new conectaDAO().connectDB();
         String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
@@ -31,7 +31,7 @@ public class ProdutosDAO {
         }
     }
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
+    public ArrayList<ProdutosDTO> listarProdutos() {
         String sql = "SELECT * FROM produtos";
         conn = new conectaDAO().connectDB();
         listagem = new ArrayList<>();
@@ -53,6 +53,22 @@ public class ProdutosDAO {
             System.out.println("Erro ao listar: " + ex.getMessage());
         }
         return listagem;
+    }
+
+    public void venderProduto(int id) {
+        conn = new conectaDAO().connectDB();
+        String sql = "UPDATE produtos SET status = ? WHERE id = ?";
+        
+        try {
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, "Vendido");
+            prep.setInt(2, id);
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Status do produto atualizado para 'Vendido'!");
+        } catch (SQLException ex) {
+            System.out.println("Erro ao vender produto: " + ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Erro ao atualizar produto.");
+        }
     }
         
 }
